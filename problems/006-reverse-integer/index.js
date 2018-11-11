25e477b3daa5016f0ec2cdfb3ed201731c89587d
Shenfq <shenfq95@foxmail.com>
2018-11-11 11:28:44 +0800

    feat: reverse-integer

---
+++ problems/006-reverse-integer/index.js
@@ -0,0 +1,42 @@
+/**
+ * 给定一个 32 位有符号整数，将整数中的数字进行反转。
+ * 示例 1:
+ * 输入: 123
+ * 输出: 321
+ *
+ * 示例 2:
+ * 输入: -123
+ * 输出: -321
+ *
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function (x) {
+  if (x == 0) {
+    return 0
+  }
+  const numStr = Math.abs(x).toString()
+  let i = numStr.length - 1
+  let result = ''
+
+  for (; i >= 0; i--) {
+    result += numStr[i]
+    if (result == 0) {
+      result = ''
+    }
+  }
+
+  result = x < 0 ? ('-' + result) : result
+  result = parseInt(result)
+  if (result < -(2 ** 31) || result > (2 ** 31 - 1)) {
+    return 0
+  }
+  return result
+};
+
+// -321
+// 21
+console.log(
+  reverse(-123),
+  reverse(120)
+)
