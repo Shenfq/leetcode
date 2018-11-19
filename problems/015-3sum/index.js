ec4e3cbe2e1fe666f3be8029b2fb5be353531940
Shenfq <shenfq95@foxmail.com>
2018-11-19 10:21:30 +0800

    feat: 三数之和

---
+++ problems/015-3sum/index.js
@@ -0,0 +1,47 @@
+/**
+ * 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，
+ * 使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
+ *
+ * 注意：答案中不可以包含重复的三元组。
+ * 例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，满足要求的三元组集合为：
+ * [
+ *   [-1, 0, 1],
+ *   [-1, -1, 2]
+ * ]
+ *
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+  nums = nums.sort((a, b) => a - b)
+  const result = []
+  for (let i = 0, len = nums.length; i < len - 2; i++) {
+    if (nums[i] === nums[i - 1]) {
+      continue
+    }
+    let l = i + 1, r = len - 1
+    while (l < r) {
+      const tmp = nums[i] + nums[l] + nums[r]
+      if (tmp > 0) {
+        r--
+      } else if (tmp < 0) {
+        l++
+      } else {
+        result.push([nums[i], nums[l], nums[r]])
+        l++
+        r--
+        while(l < r && nums[l] === nums[l - 1]) {
+          l++
+        }
+        while(l < r && nums[r] === nums[r + 1]) {
+          r--
+        }
+      }
+    }
+  }
+  return result
+};
+
+console.log(
+  threeSum([-1, 0, 1, 2, -1, -4])
+)
---
+++ problems/016-3sum-closest/index.js
@@ -0,0 +1,43 @@
+/**
+ *
+ * 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。
+ * 返回这三个数的和。假定每组输入只存在唯一答案。
+ *
+ * 例如，给定数组 nums = [-1，2，1，-4], 和 target = 1.
+ * 与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).
+ *
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+  nums = nums.sort((a, b) => a - b)
+  let ret = Number.MAX_SAFE_INTEGER
+  for (let i = 0, len = nums.length; i < len - 2; i++) {
+    if (nums[i] === nums[i - 1]) {
+      continue
+    }
+    let l = i + 1, r = len - 1
+    while (l < r) {
+      const sum = nums[i] + nums[l] + nums[r]
+      if (
+        Math.abs(sum - target) < Math.abs(ret - target)
+      ) {
+        ret = sum
+      }
+
+      if (sum < target) {
+        l++
+      } else if (sum > target) {
+        r--
+      } else {
+        return sum
+      }
+    }
+  }
+  return ret
+};
+
+console.log(
+  threeSumClosest([-1, 2, 1, -4], 1)
+)
